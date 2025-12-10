ea03b79e768bd0a1aefc13d979c5a243fb79334d
Tian Haoxiang <mtienoreki@gmail.com>
2025-12-11 00:06:08 +0800

    feat: ensure data consistency

--- src/main/handlers/index.ts
+++ src/main/handlers/index.ts
@@ -15,15 +15,18 @@ import WorkspaceLowdbService from '../services/workspace'
 import log from 'electron-log/main'
 
 export async function initHandlers(mainWindow: BrowserWindow): Promise<void> {
-    // TODO 提供一个接口处理数据和文件的一致性
-    await initExampleHandlers(mainWindow)
-    await initImageHandlers(mainWindow)
-    await initPromptHandlers(mainWindow)
-    await initTagHandlers(mainWindow)
-    await initWorkspaceHandlers(mainWindow)
-    await initOtherHandlers(mainWindow)
+    const initPromise = (async () => {
+        await initExampleHandlers(mainWindow)
+        await initImageHandlers(mainWindow)
+        await initPromptHandlers(mainWindow)
+        await initTagHandlers(mainWindow)
+        await initWorkspaceHandlers(mainWindow)
+        await initOtherHandlers(mainWindow)
+        await ensureDataConsistency(mainWindow)
+    })()
     ipcMain.once(IpcChannel.DataStoreReady, async () => {
         log.info(`recv data store ready at: ${Date.now()}`)
+        await initPromise
         mainWindow.webContents.send(
             IpcChannel.NotifyExamples,
             await ExampleLowdbService.getInstance().getAll()
@@ -45,4 +48,47 @@ export async function initHandlers(mainWindow: BrowserWindow): Promise<void> {
             await WorkspaceLowdbService.getInstance().getAll()
         )
     })
+}
+
+export async function ensureDataConsistency(_mainWindow: BrowserWindow): Promise<void> {
+    const exampleService = await ExampleLowdbService.getInstance()
+    const tagService = await TagLowdbService.getInstance()
+    const imageService = await ImageLowdbService.getInstance()
+    const promptService = await PromptLowdbService.getInstance()
+    const workspaceService = await WorkspaceLowdbService.getInstance()
+
+    const allExamples = await exampleService.getAll()
+    const allTags = await tagService.getAll()
+    const allPrompts = await promptService.getAll()
+    const allWorkspaces = await workspaceService.getAll()
+    const allImages = await imageService.getAll()
+
+    const tagIdSet = new Set(allTags.map((tag) => tag.id))
+    const exampleIdSet = new Set(allExamples.map((example) => example.id))
+    const imageIdSet = new Set(allImages.map((image) => image.id))
+    for (const example of allExamples) {
+        const newImageIds = example.imageIds.filter((id) => imageIdSet.has(id))
+        if (newImageIds.length !== example.imageIds.length) {
+            const newExample = { ...example, imageIds: newImageIds }
+            await exampleService.update([newExample])
+        }
+    }
+    for (const prompt of allPrompts) {
+        const newTagIds = prompt.tagIds.filter((id) => tagIdSet.has(id))
+        const newExampleIds = prompt.exampleIds.filter((id) => exampleIdSet.has(id))
+        if (
+            newTagIds.length !== prompt.tagIds.length ||
+            newExampleIds.length !== prompt.exampleIds.length
+        ) {
+            const newPrompt = { ...prompt, tagIds: newTagIds, exampleIds: newExampleIds }
+            await promptService.update([newPrompt])
+        }
+    }
+    for (const workspace of allWorkspaces) {
+        const newTagIds = workspace.tagIds.filter((id) => tagIdSet.has(id))
+        if (newTagIds.length !== workspace.tagIds.length) {
+            const newWorkspace = { ...workspace, tagIds: newTagIds }
+            await workspaceService.update([newWorkspace])
+        }
+    }
 }
